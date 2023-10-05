fa15456b6bb078d352e797b8fcdfe4478c799449
malikuzair <46522546+malikuzair@users.noreply.github.com>
2023-10-05 13:39:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ function greetUser(firstName, lastName) {
   const fullName = `${firstName} ${lastName}`;
   return `Hello, ${fullName}!`;
 }
+//q3//
+function addNumbers(a, b) {
+  return a + b;
+}
+
+const num1 = parseFloat(prompt("Enter first number:"));
+const num2 = parseFloat(prompt("Enter second number:"));
+
+const sum = addNumbers(num1, num2);
+
+console.log(sum);
